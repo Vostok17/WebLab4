77688ca4e84c096c3c9f24ffee04f3fa3f2a1a3a
Vostok17 <gagarin.artem.2008@gmail.com>
2022-01-12 20:57:35 +0300

    div animation is done

---
+++ Lab4/script.js
@@ -0,0 +1,171 @@
+'use strict';
+
+const animBorderWidth = 5;
+const squareSide = 20;
+const radius = 1;
+let squareInterval1;
+let squareInterval2;
+
+class Circle {
+    constructor(dx, dy, el) {
+        this.dx = dx;
+        this.dy = dy;
+        this.el = el;
+    }
+
+    setdx(dx) {
+        this.dx = dx;
+    }
+
+    setdy(dy) {
+        this.dy = dy;
+    }
+
+    getdx() {
+        return this.dx;
+    }
+
+    getdy() {
+        return this.dy;
+    }
+
+    getEl() {
+        return this.el;
+    }
+
+    calcAngle() {
+        if (!this.dx && !this.dy) {
+            const angle =
+                Math.random() * (Math.PI * (3 / 2) - Math.PI) + Math.PI;
+            this.dx = Math.cos(angle) * radius;
+            this.dy = -Math.sin(angle) * radius;
+        }
+    }
+}
+
+const playButton = document.querySelector('.play-button');
+const closeButton = document.querySelector('.close-button');
+const startButton = document.querySelector('.start-button');
+const stopButton = document.querySelector('.stop-button');
+const reloadButton = document.querySelector('.reload-button');
+const circle1 = new Circle(0, 0, document.querySelector('.circle1'));
+const circle2 = new Circle(0, 0, document.querySelector('.circle2'));
+const work = document.querySelector('.work');
+const anim = document.querySelector('.anim');
+
+const calcDistance = () => {
+    const centre1x = parseInt(circle1.el.style.top) + squareSide / 2;
+    const centre1y = parseInt(circle1.el.style.left) + squareSide / 2;
+
+    const centre2x = parseInt(circle2.el.style.top) + squareSide / 2;
+    const centre2y = parseInt(circle2.el.style.left) + squareSide / 2;
+
+    return Math.sqrt(
+        Math.pow(centre2x - centre1x, 2) + Math.pow(centre2y - centre1y, 2),
+    );
+};
+
+playButton.addEventListener('click', () => {
+    playButton.disabled = true;
+    startButton.style.display = 'block';
+    stopButton.style.display = 'none';
+    reloadButton.style.display = 'none';
+    work.style.display = 'block';
+    circle1.getEl().style.display = 'block';
+    circle2.getEl().style.display = 'block';
+
+    const rect = anim.getBoundingClientRect();
+
+    circle1.getEl().style.top = `${rect.top + animBorderWidth}px`;
+    const startLoc1 =
+        (rect.right - animBorderWidth - squareSide) * Math.random();
+    circle1.getEl().style.left = `${startLoc1}px`;
+
+    const style = `${rect.bottom - animBorderWidth - squareSide}px`;
+    circle2.getEl().style.top = style;
+    const startLoc2 =
+        (rect.right - animBorderWidth - squareSide) * Math.random();
+    circle2.getEl().style.left = `${startLoc2}px`;
+});
+
+closeButton.addEventListener('click', () => {
+    work.style.display = 'none';
+    playButton.disabled = false;
+    clearInterval(squareInterval1);
+    clearInterval(squareInterval2);
+});
+
+const moveObject = object => {
+    const rect = object.getEl().getBoundingClientRect();
+    const x = rect.left + object.getdx();
+    const y = rect.top + object.getdy();
+    object.getEl().style.top = `${y}px`;
+    object.getEl().style.left = `${x}px`;
+};
+
+const controlMovement = object => {
+    const objRect = object.getEl().getBoundingClientRect();
+    const animRect = anim.getBoundingClientRect();
+
+    if (objRect.right > animRect.right - animBorderWidth) {
+        object.dx = -object.getdx();
+    }
+
+    if (objRect.top < animRect.top + animBorderWidth) {
+        object.dy = -object.getdy();
+    }
+
+    if (objRect.bottom > animRect.bottom - animBorderWidth) {
+        object.dy = -object.getdy();
+    }
+
+    if (objRect.left < animRect.left + animBorderWidth) {
+        object.dx = -object.getdx();
+    }
+
+    if (calcDistance() <= squareSide) {
+        circle1.dx = -circle1.dx;
+        circle1.dy = -circle1.dy;
+        circle2.dx = -circle2.dx;
+        circle2.dy = -circle2.dy;
+    }
+    moveObject(object);
+};
+
+startButton.addEventListener('click', () => {
+    startButton.style.display = 'none';
+    reloadButton.style.display = 'none';
+    stopButton.style.display = 'block';
+
+    circle1.calcAngle();
+    circle2.calcAngle();
+
+    squareInterval1 = setInterval(controlMovement, 1, circle1);
+    squareInterval2 = setInterval(controlMovement, 1, circle2);
+});
+
+stopButton.addEventListener('click', () => {
+    startButton.style.display = 'block';
+    reloadButton.style.display = 'none';
+    stopButton.style.display = 'none';
+    clearInterval(squareInterval1);
+    clearInterval(squareInterval2);
+});
+
+reloadButton.addEventListener('click', () => {
+    startButton.style.display = 'block';
+    stopButton.style.display = 'none';
+    reloadButton.style.display = 'none';
+
+    const rect = anim.getBoundingClientRect();
+
+    circle1.style.top = `${rect.top + animBorderWidth}px`;
+    const startLoc1 =
+        (rect.right - animBorderWidth - squareSide) * Math.random();
+    circle1.style.left = `${startLoc1}px`;
+
+    circle2.style.top = `${rect.bottom - animBorderWidth - squareSide}px`;
+    const startLoc2 =
+        (rect.right - animBorderWidth - squareSide) * Math.random();
+    circle2.style.left = `${startLoc2}px`;
+});
